a3027f218329c123d8fc58ebb6e75d89470b0438
farrerz <farrerz@wit.edu>
2015-03-07 18:45:39 -0500

    Initial commit

---
+++ src/queue/application/Train.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queue.application;
+
+/**
+ *
+ * @author zfarrer
+ */
+public class Train {
+    private String trainNumber;
+    private int capacity;
+    private int passangerCount;
+   
+    
+}
